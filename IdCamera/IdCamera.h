972fd7f923738073f24c22ee1b5690739dfc8777
Irwan <>
2021-10-27 17:20:03 +0700

    first commit

---
+++ IdCamera/IdCamera.h
@@ -0,0 +1,18 @@
+//
+//  IdCamera.h
+//  IdCamera
+//
+//  Created by Irwan  on 27/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IdCamera.
+FOUNDATION_EXPORT double IdCameraVersionNumber;
+
+//! Project version string for IdCamera.
+FOUNDATION_EXPORT const unsigned char IdCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IdCamera/PublicHeader.h>
+
+
